4f937834512c29d5983768b10e0c56903e4dec20
alans <alans09@gmail.com>
2014-06-24 22:23:06 +0200

    Removed unittests, added kill process kw, doc fix Documentation removed due to full wiki page Every keyword returns value (almost all returns True if succeed) 	modified:   rxconnector.py 	deleted:    unit_tests/__init__.py 	deleted:    unit_tests/rxconnector_test.py

--- src/rxconnector.py
+++ src/rxconnector.py
@@ -1,34 +1,19 @@
-""" Remote ranorex library for robot framework
-Implemented keywords:
-    Click Element -> Click on element identified by xpath
-    Check -> If checkbox/radiobutton is checked, do nothing else check
-    Double Click Element -> Double clicks on element identified by xpath
-    Get Element Attribute -> Gets desired attribute of element identified
-        by xpath
-    Input Text -> Inputs desired text into element identified by xpath
-    Right Click Element -> Right click on element identified by xpath
-    Run Application -> Starts application
-    Run Application With Parameters -> Start application with added parameters
-    Run Script -> Run script and return {'stdout':'<stdout>',
-        'stderr':'<stderr>'}
-    Run Script With Parameters -> Run script with parameters
-        and returns {'stdout':'<stdout>', 'stderr':'<stderr>'}
-    Select By Index -> Select value from combobox according to index
-    Take Screenshot -> Takes screenshot of desired element identified by xpath
-        and returns base64 encoded string
-    Uncheck -> Checks if radiobutton/checkbox is checked.. if so it uncheck it
-    Wait For Element -> Waits for desired element for desired time
-    Wait For Element Attribute -> Waits for specified attribute of desired
-        element
 """
+    Remote ranorex library for robot framework
+    All commands return True if they are executed correctly
+"""
+#iron python imports
 import clr
 clr.AddReference('Ranorex.Core')
 clr.AddReference('System.Windows.Forms')
 import System.Windows.Forms
 import Ranorex
+#python imports
 import sys
-import os
+import subprocess
+import time
 from robotremoteserver import RobotRemoteServer
+
 
 class RanorexLibrary(object):
     """ Basic implementation of ranorex object calls for
@@ -38,10 +23,7 @@ class RanorexLibrary(object):
     def __return_type(cls, locator):
         """ Function serves as translator from xpath into
         .net object that is recognized by ranorex.
-        Inputs:
-            locator -> xpath to element
-        Output:
-            string with .net object type
+        Returns supported object type.
         """
         Ranorex.Validate.EnableReport = False
         Ranorex.Adapter.DefaultUseEnsureVisible = True
@@ -73,42 +55,36 @@ class RanorexLibrary(object):
                 raise AssertionError("No element entered")
         raise AssertionError("Element is not supported. Entered element: %s" %
                              ele)
- 
+
     def click_element(self, locator, location=None):
-        """ Clicks on element located on location.
-        Inputs:
-            locator ->  xpath to object
-            location -> relative coordinates to click (comma separated string)
-        Output:
-            Nothing if success, else RanorexException raised
+        """ Clicks on element identified by locator and location
         """
         element = self.__return_type(locator)
         try:
             if location == None:
                 getattr(Ranorex, element)(locator).Click()
+                return True
             else:
                 if not isinstance(location, basestring):
-                    raise AssertionError('Location must be a string')
+                    raise AssertionError("Location must be a string")
                 location = [int(x) for x in location.split(',')]
                 ele = getattr(Ranorex, element)(locator)
                 ele.Click(Ranorex.Location(location[0], location[1]))
+                return True
         except Exception as error:
             raise AssertionError(error)
 
     def check(self, locator):
-        """ Check if element is checked. If not it check it
-        Inputs:
-            locator -> xpath to object
-        Output:
-            Nothing if success
-            If other element than CheckBox / RadioButton error
-            else RanorexException
+        """ Check if element is checked. If not it check it.
+            Only checkbox and radiobutton are supported.
+            Uses Click() method to check it.
         """
         element = self.__return_type(locator)
         if element == 'CheckBox' or element == 'RadioButton':
             obj = getattr(Ranorex, element)(locator)
             if not obj.Element.GetAttributeValue('Checked'):
                 obj.Click()
+                return True
         else:
             raise AssertionError("Element |%s| is not supported for checking" %
                                  element)
@@ -116,25 +92,14 @@ class RanorexLibrary(object):
     @classmethod
     def check_if_process_is_running(cls, process_name):
         """ Check if process with desired name is running.
-        Inputs:
-            process_name -> name of process to look for
-        Output:
-            False if not found
-            name if running
+            Returns name of process if running
         """
-        import subprocess
         proc = subprocess.Popen(['tasklist'], stdout=subprocess.PIPE)
         out = proc.communicate()[0]
         return out.find(process_name) != -1 if out else False
 
     def clear_text(self, locator):
         """ Clears text from text box. Only element Text is supported.
-        Inputs:
-            locator -> xpath to object
-        Output:
-            Nothing if ok
-            AssertionError if not supported element
-            else RanorexException
         """
         element = self.__return_type(locator)
         if element != "Text":
@@ -142,105 +107,78 @@ class RanorexLibrary(object):
         else:
             obj = getattr(Ranorex, element)(locator)
             obj.PressKeys("{End}{Shift down}{Home}{Shift up}{Delete}")
+            return True
+        raise AssertionError("Element %s does not exists" % locator) 
 
     def double_click_element(self, locator, location=None):
-        """ Doubleclick on element identified by xpath.
-        Inputs:
-            locator -> xpath to object
-            location -> relative coordinates to click (comma separated string)
-        Output:
-            Nothing if success
-            else RanorexException
+        """ Doubleclick on element identified by locator. It can click
+            on desired location if requested.
         """
         element = self.__return_type(locator)
         try:
             if location == None:
                 getattr(Ranorex, element)(locator).DoubleClick()
+                return True
             else:
                 if not isinstance(location, basestring):
-                    raise AssertionError('Location must be a string')
+                    raise AssertionError("Location must be a string")
                 location = [int(x) for x in location.split(',')]
                 ele = getattr(Ranorex, element)(locator)
                 ele.DoubleClick(Ranorex.Location(location[0], location[1]))
+                return True
         except Exception as error:
             raise AssertionError(error)
 
     def get_element_attribute(self, locator, attribute):
         """ Get specified element attribute.
-        Inputs:
-            locator -> xpath to object
-            attribute -> attribute to get
-        Outputs:
-            If attribute exists output its value
-            else RanorexException"""
+        """
         element = self.__return_type(locator)
         obj = getattr(Ranorex, element)(locator)
         return obj.Element.GetAttributeValue(attribute)
 
     def input_text(self, locator, text):
         """ input texts into specified locator.
-        Inputs:
-            locator -> xpath to object
-            text -> text to enter
-        Outputs:
-            Nothing if success
-            else RanorexException
         """
         element = self.__return_type(locator)
         getattr(Ranorex, element)(locator).PressKeys(text)
+        return True
 
     def right_click_element(self, locator, location=None):
-        """ Rightclick on desired element.
-        Inputs:
-            locator -> xpath to object
-            location -> relative coordinates to click (comma separated string)
-        Outputs:
-            Nothing if success
-            else RanorexException
+        """ Rightclick on desired element identified by locator.
+        Location of click can be used.
         """
         element = self.__return_type(locator)
         obj = getattr(Ranorex, element)(locator)
         if location == None:
             obj.Click(System.Windows.Forms.MouseButtons.Right)
+            return True
         else:
             if not isinstance(location, basestring):
-                raise AssertionError("Locator must be a string")
+                rtatus:q
+                ise AssertionError("Locator must be a string")
             location = [int(x) for x in location.split(',')]
             obj.Click(System.Windows.Forms.MouseButtons.Right,
                       Ranorex.Location(location[0], location[1]))
+            return True
 
     @classmethod
     def run_application(cls, app):
         """ Runs local application.
-        Inputs:
-            app -> application to start (path to it)
-        Outputs:
-            nothing if success
-            else RanorexException
         """
         Ranorex.Host.Local.RunApplication(app)
+        return True
 
     @classmethod
     def run_application_with_parameters(cls, app, params):
         """ Runs local application with parameters.
-        Inputs:
-            app -> application to start
-            params -> parameters for this application
-        Outputs:
-            Nothing if success
-            else RanorexException
         """
         Ranorex.Host.Local.RunApplication(app, params)
+        return True
 
     @classmethod
     def run_script(cls, script_path):
         """ Runs script on remote machine and returns stdout and stderr.
-        Inputs:
-            script_path -> path to script on local harddisk
-        Outputs:
-            dictionary {'stdout':<stdout>, 'stderr':<stderr>}
         """
-        import subprocess
         process = subprocess.Popen([script_path],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
@@ -250,13 +188,7 @@ class RanorexLibrary(object):
     @classmethod
     def run_script_with_parameters(cls, script_path, params):
         """ Runs script on remote machine and returns stdout and stderr.
-        Inputs:
-            script_path -> path to script on local harddisk
-            params -> parameters to it
-        Outputs:
-            dictionary {'stdout':<stdout>, 'stderr':<stderr>}
         """
-        import subprocess
         process = subprocess.Popen([script_path, params],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.PIPE)
@@ -265,12 +197,6 @@ class RanorexLibrary(object):
 
     def select_by_index(self, locator, index):
         """ Selects item from combobox according to index.
-        Inputs:
-            locator -> xpath to object
-            index -> index in combobox
-        Outputs:
-            Nothing if success
-            else RanorexException
         """
         element = self.__return_type(locator)
         obj = getattr(Ranorex, element)(locator)
@@ -282,7 +208,7 @@ class RanorexLibrary(object):
         elif diff < 0:
             for _ in range(0, abs(diff)):
                 obj.PressKeys("{down}")
-        return diff
+        return True
 
     @classmethod
     def send_keys(cls, locator, key_seq):
@@ -290,23 +216,13 @@ class RanorexLibrary(object):
         Also it gets focus before executing sequence
         seq according to :
         http://msdn.microsoft.com/en-us/library/system.windows.forms.keys.aspx
-        Inputs:
-            locator -> xpath to object
-            key_seq -> key sequence e.g. {Control down}{SKey}{Control up}
-        Output:
-            Nothing if success
-            else RanorexException
         """
         Ranorex.Keyboard.PrepareFocus(locator)
         Ranorex.Keyboard.Press(key_seq)
+        return True
 
     def set_focus(self, locator):
         """ Sets focus on desired location.
-        Inputs:
-            locator -> xpath to object
-        Outputs:
-            True if focus is set
-            else False
         """
         element = self.__return_type(locator)
         obj = getattr(Ranorex, element)(locator)
@@ -315,11 +231,6 @@ class RanorexLibrary(object):
 
     def take_screenshot(self, locator):
         """ Takes screenshot and return it as base64.
-        Inputs:
-            locator -> xpath to object
-        Outputs:
-            base64string if success
-            else RanorexException
         """
         element = self.__return_type(locator)
         obj = getattr(Ranorex, element)(locator)
@@ -328,18 +239,13 @@ class RanorexLibrary(object):
 
     def uncheck(self, locator):
         """ Check if element is checked. If yes it uncheck it
-        Inputs:
-            locator -> xpath to object
-        Output:
-            Nothing if success
-            Error not supported element if other than CheckBox/RadioButton
-            else RanorexException
         """
         element = self.__return_type(locator)
         if element == 'CheckBox' or element == 'RadioButton':
             obj = getattr(Ranorex, element)(locator)
             if obj.Element.GetAttributeValue('Checked'):
                 obj.Click()
+                return True
         else:
             raise AssertionError("Element |%s| not supported for unchecking"
                                  % element)
@@ -356,45 +262,28 @@ class RanorexLibrary(object):
         """
         Ranorex.Validate.EnableReport = False
         if Ranorex.Validate.Exists(locator, int(timeout)):
-            return 'Element %s exists' % locator
+            return True
+        raise AssertionError("Element %s does not exists" % locator)
 
     def wait_for_element_attribute(self, locator, attribute,
                                    expected, timeout):
         """ Wait for element attribute becomes requested value.
-        Inputs:
-            locator -> xpath to object
-            attribute -> object attribute
-            expected -> expected value of attribute
-            timeout -> timeout in ms to wait for object
-        Outputs:
-            True if success
-            FAIL if not found
-            else RanorexException
         """
-        import time
         curr_time = 0
         timeout = int(timeout)/1000
         while curr_time != timeout:
             value = self.get_element_attribute(locator, attribute)
             if str(value) == str(expected):
                 return True
-            time.sleep(1)
-            curr_time += 1
+            time.sleep(5)
+            curr_time += 5
         raise AssertionError("Object at location %s could not be found"
                              % locator)
 
     @classmethod
     def wait_for_process_to_start(cls, process_name, timeout):
         """ Waits for /timeout/ seconds for process to start.
-        Input:
-            process_name -> name of process to wait for
-            timeout -> timeout in ms to wait
-        Output:
-            True if process is started
-            False if process is not started within specified time
         """
-        import time
-        import subprocess
         curr_time = 0
         timeout = int(timeout)/1000
         while curr_time <= timeout:
@@ -406,9 +295,22 @@ class RanorexLibrary(object):
             else:
                 curr_time += 5
                 time.sleep(5)
-        raise AssertionError('Process %s not found within %ss' % (process_name,
+        raise AssertionError("Process %s not found within %ss" % (process_name,
                                                                   timeout))
 
+    def kill_process(self, process_name):
+        """ Kills process identified by process_name
+        """
+        if not self.check_if_process_is_running(process_name):
+            raise AssertionError("Process %s is not running" % process_name)
+        proc = subprocess.Popen(['taskkill', '/im', process_name, '/f'],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out = proc.communicate()
+        if 'SUCCESS' in out[0]:
+            return True
+        else:
+            raise AssertionError("Process %s not terminated because of: %s" %
+                                 (process_name, out))
 
 if __name__ == '__main__':
     RobotRemoteServer(RanorexLibrary(), *sys.argv[1:])
