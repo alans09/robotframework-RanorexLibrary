4120b8b013d108d419a5eb2eb1d7e97dcabc044c
alans09 <alans09@gmail.com>
2014-09-22 10:23:55 +0200

    Renamed move_to to move_mouse_to

--- src/rxconnector.py
+++ src/rxconnector.py
@@ -231,13 +231,15 @@ class RanorexLibrary(object):
         return True
 
     @classmethod
-    def move_to(cls, x, y):
+    def move_mouse_to(cls, x, y):
         """ Move mouse to global coordinates
 
         :param x: int, position on x axis
         :param y: int, position on y axis
         :return: True on success
         """
+        if self.debug:
+            log.debug("Moving mouse to: %s,%s", x, y)
         mouse = Ranorex.Mouse()
         mouse.MoveTo(int(x), int(y))
         return True
